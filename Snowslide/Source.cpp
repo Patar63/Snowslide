6cb6b97aba8cda51867c4f3079499a349c5332b6
Ewan <ewwesley2@gmail.com>
2020-10-23 18:40:04 -0400

    Press Enter to start

--- Snowslide/Source.cpp
+++ Snowslide/Source.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 char input;
 
 int main()
 {
+	string enterPressed;
 	int climb = 5;
 	int layer = 1;
 
 	cout << "You are a new hiker, determined to climb the mountain in front of you. Good luck, and be careful of snowslides.\n";
-	cout << "Press X to start...\n";
-	cin >> input;
+	cout << "Press Enter to start...\n";
+	getline(cin, enterPressed);
 
-	if (input == 'x')
+	if (enterPressed.empty())
 	{
-
+		cout << "Enter was pressed\n";
+	}
+	else
+	{
+		cout << "A letter was entered with enter\n";
 	}
 }
