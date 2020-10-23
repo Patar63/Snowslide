fd1749c4a8e75c9044fee300afcbf3ab39a05657
Patrick Thompson <patrick.thompson8@ontariotechu.net>
2020-10-23 18:30:56 -0400

    Beginning of code

---
+++ Snowslide/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+char input;
+
+int main()
+{
+	int climb = 5;
+	int layer = 1;
+
+	cout << "You are a new hiker, determined to climb the mountain in front of you. Good luck, and be careful of snowslides.\n";
+	cout << "Press X to start...\n";
+	cin >> input;
+
+	if (input == 'x')
+	{
+
+	}
+}
