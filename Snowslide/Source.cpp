0f568ce7eb5afe53987e50cd678ade186f4b5210
Patrick Thompson <patrick.thompson8@ontariotechu.net>
2020-10-25 19:26:22 -0400

    MAde it correct position if the player overshoots, and added a function if a player gives a non existent card value

--- Snowslide/Source.cpp
+++ Snowslide/Source.cpp
@@ -2,22 +2,24 @@
 #include <string>
 using namespace std;
 
-char input;
 int movement(int, int, int, int, int);
+int selection(int);
+
+//variable to track how far from climb spot
+int climb = 5;
+//variable to track which level you're on
+int layer = 1;
+char input;
 
 int main()
 {
 	//initializes the needed variables
 	string enterPressed;
-	//variable to track how far from climb spot
-	int climb = 5;
-	//variable to track which level you're on
-	int layer = 1;
 	int card1;
 	int card2;
 	int card3;
 	//store place for value of which card gets played by the player
-	int play;
+	int play{};
 
 	//starts up game so player can choose when to begin
 	cout << "You are a new hiker, determined to climb the mountain in front of you. Good luck, and be careful of snowslides.\n";
@@ -33,20 +35,47 @@ int main()
 
 		//gets the wanted move spaces from player 
 		cout << "GAME START!!!\n";
-		cout << "You are currently " << climb << " spaces from the next rope spot. How far would you like to move?\n";
+		if (climb > 0)
+		{
+			cout << "You are currently " << climb << " spaces from the next rope spot. How far would you like to move?\n";
+		}
+		else
+		{
+			cout << "You are on a climb space! To ascend to the next level, you must play a 1";
+		}
 		cout << "1: " << card1 << "\n";
 		cout << "2: " << card2 << "\n";
 		cout << "3: " << card3 << "\n";
-		cin >> play;
 
+		//runs the function to pick cards
+		selection(play);
+		
 		//runs the movement function
-		movement(play, climb, card1, card2, card3);
+		if (play == 1 || play == 2 || play == 3)
+		{
+			movement(play, climb, card1, card2, card3);
+		}
 	}
 	else
 	{
-		cout << "Sorry, but you pressed the wrong key";
+		cout << "Sorry, but you pressed a wrong key";
 	}
 }
+
+
+int selection(int play)
+{
+	cin >> play;
+
+	if (play > 3 || play < 1)
+	{
+		cout << "You must pick a useable card.";
+		selection(play);
+	}
+
+	return play;
+}
+
 
 int movement(int play, int climb, int card1, int card2, int card3)
 {
@@ -55,6 +84,7 @@ int movement(int play, int climb, int card1, int card2, int card3)
 	{
 		climb = climb - card1;
 		
+		//corrects position if they go past the climb space
 		if (climb < 0)
 		{
 			climb = climb + 6;
