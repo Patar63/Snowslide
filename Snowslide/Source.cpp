504887b7146d9d8789bb3f0433d2792274d5ef93
Josh <jkeny127@gmai.com>
2020-10-25 19:24:09 -0400

    Added individual spaces
    Tells the player what space they landed on and what it does

--- Snowslide/Source.cpp
+++ Snowslide/Source.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 char input;
-int movement(int, int, int, int, int);
+int movement(int, int, int, int, int, int, int);
 
 int main()
 {
 	//initializes the needed variables
 	string enterPressed;
 	//variable to track how far from climb spot
-	int climb = 5;
+	int climb = 2;
 	//variable to track which level you're on
 	int layer = 1;
 	int card1;
 	int card2;
 	int card3;
+
+	//variable to track which space the player lands on
+	int space = { 0 };
 	//store place for value of which card gets played by the player
 	int play;
 
@@ -40,7 +43,7 @@ int main()
 		cin >> play;
 
 		//runs the movement function
-		movement(play, climb, card1, card2, card3);
+		movement(play, climb, card1, card2, card3, layer, space);
 	}
 	else
 	{
@@ -48,28 +51,223 @@ int main()
 	}
 }
 
-int movement(int play, int climb, int card1, int card2, int card3)
+int movement(int play, int climb, int card1, int card2, int card3, int layer, int space)
 {
-	//changes climb based on which card is played
-	if (play == 1)
-	{
-		climb = climb - card1;
-		
-		if (climb < 0)
-		{
-			int overflow;
-			overflow = climb * -1;
+	int overflow;
 
+	if (layer == 1)
+	{
+		//if the player played card 1
+		if (play == 1)
+		{
+			//adds the card number to the space number
+			space = space + card1;
+
+			if (space == 0 || space == 4 || space == 8 || space == 12 || space == 16)
+			{
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a black space. Nothing happens.";
+			}
+
+			if (space == 1 || space == 5 || space == 9 || space == 13 || space == 17)
+			{
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a blue space. Take a rest.";
+			}
+
+			if (space == 2 || space == 6 || space == 10 || space == 14 || space == 18)
+			{
+				//tells the system that the player is on a climb space
+				climb = 0;
+
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a climb space. You are now on layer 2.";
+
+				if (space == 2)
+				{
+					space = 0;
+				}
+
+				if (space == 6)
+				{
+					space = 3;
+				}
+
+				if (space == 10)
+				{
+					space = 6;
+				}
+
+				if (space == 14)
+				{
+					space = 9;
+				}
+
+				if (space == 18)
+				{
+					space = 12;
+				}
+
+				layer++;
+			}
+			
+			if (climb < 0)
+			{ 
+				overflow = climb * -1;
+			}
+		}
+
+		//if the player played card 2
+		if (play == 2)
+		{
+			//adds the card number to the space number
+			space = space + card2;
+
+			if (space == 0 || space == 4 || space == 8 || space == 12 || space == 16)
+			{
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a black space. Nothing happens.";
+			}
+
+			if (space == 1 || space == 5 || space == 9 || space == 13 || space == 17)
+			{
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a blue space. Take a rest.";
+			}
+
+			if (space == 2 || space == 6 || space == 10 || space == 14 || space == 18)
+			{
+				//tells the system that the player is on a climb space
+				climb = 0;
+
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a climb space. You are now on layer 2.";
+
+				if (space == 2)
+				{
+					space = 0;
+				}
+
+				if (space == 6)
+				{
+					space = 3;
+				}
+
+				if (space == 10)
+				{
+					space = 6;
+				}
+
+				if (space == 14)
+				{
+					space = 9;
+				}
+
+				if (space == 18)
+				{
+					space = 12;
+				}
+
+				layer++;
+			}
+
+			if (climb < 0)
+			{
+				overflow = climb * -1;
+			}
+		}
+
+		//if the player played card 3
+		if (play == 3)
+		{
+			//adds the card number to the space number
+			space = space + card3;
+
+			if (space == 0 || space == 4 || space == 8 || space == 12 || space == 16)
+			{
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a black space. Nothing happens.";
+			}
+
+			if (space == 1 || space == 5 || space == 9 || space == 13 || space == 17)
+			{
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a blue space. Take a rest.";
+			}
+
+			if (space == 2 || space == 6 || space == 10 || space == 14 || space == 18)
+			{
+				//tells the system that the player is on a climb space
+				climb = 0;
+
+				//tells player where they landed and what happens
+				std::cout << "You have landed on a climb space. You are now on layer 2.";
+
+				if (space == 2)
+				{
+					space = 0;
+				}
+
+				if (space == 6)
+				{
+					space = 3;
+				}
+
+				if (space == 10)
+				{
+					space = 6;
+				}
+
+				if (space == 14)
+				{
+					space = 9;
+				}
+
+				if (space == 18)
+				{
+					space = 12;
+				}
+
+				layer++;
+			}
+
+			if (climb < 0)
+			{
+				overflow = climb * -1;
+			}
 		}
 	}
-	else if (play == 2)
+
+	if (layer == 2)
 	{
-		climb = climb - card2;
+
 	}
-	else if (play == 3)
+
+	if (layer == 3)
 	{
-		climb = climb - card3;
+
 	}
+
+	////changes climb based on which card is played
+	//if (play == 1)
+	//{
+	//	climb = climb - card1;
+	//	
+	//	if (climb < 0)
+	//	{
+	//		int overflow;
+	//		overflow = climb * -1;
+
+	//	}
+	//}
+	//else if (play == 2)
+	//{
+	//	climb = climb - card2;
+	//}
+	//else if (play == 3)
+	//{
+	//	climb = climb - card3;
+	//}
 
 	//retruns value for where the player is on the board
 	return climb;
